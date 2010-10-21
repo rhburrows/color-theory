9815d141fb023e263d57368d6f4c7816408e10a8
Ryan Burrows <rhburrows@crystalcommerce.com>
2010-10-21 08:08:33 -0700

    Add more colors

--- lib/color-theory.rb
+++ lib/color-theory.rb
@@ -2,9 +2,20 @@ require 'color-theory/color'
 require 'color-theory/spectrum'
 
 module ColorTheory
-  RED   = Color.new(255, 0, 0)
-  GREEN = Color.new(0, 255, 0)
-  BLUE  = Color.new(0, 0, 255)
-  BLACK = Color.new(0, 0, 0)
-  WHITE = Color.new(255, 255, 255)
+  WHITE   = Color.new(255, 255, 255)
+  SILVER  = Color.new(192, 192, 192)
+  GRAY    = Color.new(128, 128, 128)
+  BLACK   = Color.new(0, 0, 0)
+  RED     = Color.new(255, 0, 0)
+  MAROON  = Color.new(128, 0, 0)
+  YELLOW  = Color.new(255, 255, 0)
+  OLIVE   = Color.new(128, 128, 0)
+  LIME    = Color.new(0, 255, 0)
+  GREEN   = Color.new(0, 128, 0)
+  AQUA    = Color.new(0, 255, 255)
+  TEAL    = Color.new(0, 128, 128)
+  BLUE    = Color.new(0, 0, 255)
+  NAVY    = Color.new(0, 0, 128)
+  FUCHSIA = Color.new(255, 0, 255)
+  PURPLE  = Color.new(128, 0, 128)
 end
